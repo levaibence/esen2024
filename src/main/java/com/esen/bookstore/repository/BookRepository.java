e74f3a343a904516e3eb34caebc132caf364882c
levaibence <levaibence@gmail.com>
2024-04-16 08:32:12 +0200

    repository

---
+++ src/main/java/com/esen/bookstore/repository/BookRepository.java
@@ -0,0 +1,14 @@
+package com.esen.bookstore.repository;
+
+import com.esen.bookstore.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+    List<Book> findByAuthor(String author);
+
+}
